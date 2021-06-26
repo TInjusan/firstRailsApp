5dcd9bb6120e0d0e455cbbaea89a35b26c94e798
TInjusan <jaztyn72@gmail.com>
2021-06-27 02:18:54 +0800

    Model validation before saving to the database

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
-   
+    #Validation to check the number of characters and if blank or not
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
   
