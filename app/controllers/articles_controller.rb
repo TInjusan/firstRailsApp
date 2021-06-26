a2e16c2429b66c701724f6455e1acf087245d50e
TInjusan <jaztyn72@gmail.com>
2021-06-27 03:00:59 +0800

    Displaying the Articles object list to the view of the web app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,13 @@ class ArticlesController < ApplicationController
     def show
         #Make it as instance variable by adding @ to the variable so that it will be accessible
         # the params[:id] gets the id in the url (the last part of the GET HTTP request) http://localhost:3000/articles/1
+      
         @article = Article.find(params[:id])
     end
     
+    def index 
+        @articles = Article.all
+    end
+
 
 end
