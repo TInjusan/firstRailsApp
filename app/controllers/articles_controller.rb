89634c4f62d4747a6743a0a49b8c1f8735e558f5
TInjusan <jaztyn72@gmail.com>
2021-06-27 03:31:30 +0800

    Saving new article to the database and redirecting to article details page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,20 @@ class ArticlesController < ApplicationController
     end
      
     def create 
+        #Due to security feature of Rails, you need to specify what you want to get from the submitted form
+        #Here's the syntax on how to do that, because if you don't do this, it will display an error
+
+        @article = Article.new(params.require(:article).permit(:title, :description))
+
+        #to render on the new article page, need to convert to json format
+        #render plain: params[:article].to_json
+
+        #command to save the gathered submitted form for new article
+        @article.save
+
+        #Redirecting to the article details page to show new article
+        redirect_to article_path(@article)
+
     end  
 
 
