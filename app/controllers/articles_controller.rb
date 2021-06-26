28c6da808eef22528e4e3da500e4a68e702f4aec
TInjusan <jaztyn72@gmail.com>
2021-06-27 02:45:49 +0800

    Getting article id and use it to display other details of the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
     def show
-        
+        #Make it as instance variable by adding @ to the variable so that it will be accessible
+        # the params[:id] gets the id in the url (the last part of the GET HTTP request) http://localhost:3000/articles/1
+        @article = Article.find(params[:id])
     end
     
 
