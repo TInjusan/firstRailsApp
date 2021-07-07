d845ebb62da03ed26440dcff3426d0e1203daa57
TInjusan <jaztyn72@gmail.com>
2021-07-07 19:46:46 +0800

    Correcting the timestamp error on deploy 2

--- db/migrate/20210626173218_create_articles.rb
+++ db/migrate/20210626173218_create_articles.rb
@@ -3,6 +3,8 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.string :title 
       t.text :description 
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
     end
   end
 end
