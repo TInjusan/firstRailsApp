9c7a7cf5eacf62783c99a5f137f9823492aff886
TInjusan <jaztyn72@gmail.com>
2021-07-08 19:31:14 +0800

    Adding columns via migration file

--- db/migrate/20210626173218_create_articles.rb
+++ db/migrate/20210626173218_create_articles.rb
@@ -3,9 +3,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.string :title 
       t.text :description 
-      t.datetime :created_at
-      t.datetime :updated_at
-
+      t.timestamps
     end
   end
 end
