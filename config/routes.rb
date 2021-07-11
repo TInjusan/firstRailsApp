b79ddf5d8e9cb8706201244f9c154891fd9ba6c0
TInjusan <jaztyn72@gmail.com>
2021-07-11 22:57:47 +0800

    Adding login for and session controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :articles , only: [:show, :index,  :new, :create, :edit, :update, :destroy  ]
   resources :users, except: [:new]
+
+  #For the login session of a user
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
 end
