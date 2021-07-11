3c58392e0a8c2e827ae34eaf3578025ab6ccd516
TInjusan <jaztyn72@gmail.com>
2021-07-11 19:06:26 +0800

    Adding validations in User model (table)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
     has_many :articles
+    
+    #Converting all charaters in the email to lowercase
+    before_save { self.email = email.downcase }
+
+    validates :username, presence: true,   #Making sure the username is not blank
+
+    uniqueness: { case_sensitive: false }, #Making sure that this is not a duplicate username
+    length: { minimum: 3, maximum: 25 }    #Making sure that email address is valid withi min and maximum no. of characters
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+    validates :email, presence: true, 
+        uniqueness: { case_sensitive: false }, 
+        length: { maximum: 105 },
+        format: { with: VALID_EMAIL_REGEX }
 end
   
