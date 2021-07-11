617fdef34015c5ab5590ca88f0faaf6fd898536e
TInjusan <jaztyn72@gmail.com>
2021-07-11 22:46:05 +0800

    Pagination for the list of articles and list of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
     def show
       @user = User.find(params[:id])
-      @articles = @user.articles
+      @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
 
     def index
-      @users = User.all
+      @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def new
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       if @user.save
         flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-        redirect_to articles_path
+        redirect_to users_path
       else
         render 'new'
       end
